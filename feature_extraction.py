75d718a99ca787a2816219f4b00b8296851852b2
darrenz78 <darrenz78@hotmail.com>
2019-08-09 21:58:44 +1000

    Feature Extraction TFIDF

---
+++ feature_extraction.py
@@ -0,0 +1,21 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+
+test = [["good", "movie"], ["not", "a", "good", "movie"], ["did", "not", "like"], ["i", "like", "it"], ["good", "one"]]
+
+def compute_tfidf(document):
+    tfidf = TfidfVectorizer(min_df=2, max_df=0.5, ngram_range=(1,3))
+
+    lines = []
+    for text in document:
+        text = " ".join(text)
+        lines.append(text)
+
+    features = tfidf.fit_transform(lines)
+    df = pd.DataFrame(features.toarray(), columns=tfidf.get_feature_names())
+
+    print(df.values)
+
+
+if __name__ == "__main__":
+    compute_tfidf(test)
